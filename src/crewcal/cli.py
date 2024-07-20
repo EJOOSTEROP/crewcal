7f7c95f31b95e330a094acb741f2e0f0fd2cfdf9
Erik Oosterop <ni7h4txi@duck.com>
2024-07-20 01:38:06 +0000

    Fix typo in help.

--- src/crewcal/cli.py
+++ src/crewcal/cli.py
@@ -146,7 +146,7 @@ def extract(sourcefile: str, targetfile: str, to_json: bool, overwrite: bool) ->
     "--nooverwrite",
     "-no",
     is_flag=True,
-    help="Don not convert if target folder already exists.",
+    help="Do not convert if target folder already exists.",
 )
 @click.argument("sourcefile")
 @click.argument("targetfolder")
